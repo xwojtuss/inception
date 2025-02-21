0de4c93b2ba81701ed8a32bacc1ef62c273a4794
Linux User <wkornato@wkornato.42.fr>
2025-02-21 18:02:46 +0100

    idk

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,55 +1,10 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
- *
- * @package WordPress
- */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
-
-/** Database username */
-define( 'DB_USER', 'wordpress' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'wordpresspassword' );
-
-/** Database hostname */
-define( 'DB_HOST', 'db:3306' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
-
-define( 'DB_SOCKET', '' ); // Force TCP/IP instead of socket connection
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set('display_errors', 0);
+define('SCRIPT_DEBUG', true);
 
 define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
 
@@ -59,53 +14,23 @@ define('WP_SITEURL', 'wkornato.42.fr');
 
 define('PHPMAILER_INIT_SMTP', false);
 
-define('DB_SSL_KEY', '/etc/ssl/private/mariadb-server-key.pem');
-define('DB_SSL_CERT', '/etc/ssl/certs/mariadb-server-cert.pem');
-define('DB_SSL_CA', '/etc/ssl/certs/ca-cert.pem');
+define('MYSQL_SSL_CA', '/etc/ssl/certs/ca-cert.pem');
+define('MYSQL_SSL_CERT', '/etc/ssl/certs/mariadb-client-cert.pem');
+define('MYSQL_SSL_KEY', '/etc/ssl/private/mariadb-client-key.pem');
 
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpresspassword' );
+define( 'DB_HOST', 'db' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 
-/**#@-*/
+define( 'DB_SOCKET', '' );
 
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- *
- * At the installation time, database tables are created with the specified prefix.
- * Changing this value after WordPress is installed will make your site think
- * it has not been installed.
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
- */
 $table_prefix = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
- */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', false );
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-/** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
